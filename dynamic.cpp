5a80121173377f59c97f2aa7a30a60f0d2a1d375
Hunor Csapó <104154290+cshunor02@users.noreply.github.com>
2025-12-14 12:20:04 +0100

    Initial commit
    This commit uses the Elliptic Curve package for C++ by Debrup Chatterjee.

---
+++ dynamic.cpp
@@ -0,0 +1,30 @@
+﻿// dynamic.cpp : Defines the entry point for the application.
+//
+
+#include "dynamic.h"
+#include "ecdsa.hpp"
+#include "ecc.hpp"
+#include "bigint.hpp"
+#include <string>
+
+using namespace std;
+
+int main() {
+    auto [publicKey, privateKey] = ECDH::generateKeyPair();
+
+    std::string message = "Hello, world!";
+
+    std::vector<unsigned char> data(message.begin(), message.end());
+
+    auto signature = ECDSA::sign(privateKey, data);
+    bool isValid = ECDSA::verify(publicKey, data, signature);
+
+    if (isValid) {
+        std::cout << "Signature is valid." << std::endl;
+    }
+    else {
+        std::cout << "Signature is invalid." << std::endl;
+    }
+
+    return 0;
+}
